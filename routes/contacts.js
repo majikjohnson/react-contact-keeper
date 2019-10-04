a0815c79f645c72dc94ea509fb9bf0f8f9175369
Matt Johnson <majikjohnson@hotmail.com>
2019-10-04 21:49:48 +0100

    Added Contact model and set up api route for retrieving all contacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
+const auth = require('../middleware/auth');
+const User = require('../models/User');
+const Contact = require('../models/Contact');
 
 const router = express.Router();
 
 // @ROUTE   GET /api/contacts
 // @DESC    Returns all contacts
 // @ACCESS  private
-router.get('/', (req, res) => res.send('Get contacts'));
+router.get('/', auth, async (req, res) => {
+    try {
+        const contacts = await Contact.find({user: req.user.id}).sort({date: -1});
+        res.json(contacts);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({msg: 'Internal Server Error'});
+    }
+});
 
 // @ROUTE   POST /api/contacts
 // @DESC    Creates a new contacts
