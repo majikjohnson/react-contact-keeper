4bc9502b0f94ee088dfacfd98db3ff60c0aa2c3a
Matt Johnson <majikjohnson@hotmail.com>
2019-10-17 08:51:50 +0100

    Basic Add & Delete animations using react-transition-group

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,24 +1,39 @@
 import React, { Fragment, useContext } from 'react';
 import ContactContext from '../../context/contacts/ContactContext';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import ContactItem from './ContactItem';
 
 const Contacts = () => {
 	const contactContext = useContext(ContactContext);
-    const { contacts, filtered } = contactContext;
-    
-    if (contacts.length === 0) {
-        return <h3>Please add a contact</h3>;
-    }
+	const { contacts, filtered } = contactContext;
+
+	if (contacts.length === 0) {
+		return <h3>Please add a contact</h3>;
+	}
 
 	return (
 		<Fragment>
-			{filtered !== null
-				? filtered.map(contact => (
-						<ContactItem key={contact.id} contact={contact} />
-				  ))
-				: contacts.map(contact => (
-						<ContactItem key={contact.id} contact={contact} />
-				  ))}
+			<TransitionGroup>
+				{filtered !== null
+					? filtered.map(contact => (
+							<CSSTransition
+								key={contact.id}
+								timeout={500}
+								classNames="item"
+							>
+								<ContactItem contact={contact} />
+							</CSSTransition>
+					  ))
+					: contacts.map(contact => (
+							<CSSTransition
+								key={contact.id}
+								timeout={500}
+								classNames="item"
+							>
+								<ContactItem contact={contact} />
+							</CSSTransition>
+					  ))}
+			</TransitionGroup>
 		</Fragment>
 	);
 };
