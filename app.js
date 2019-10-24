e1a76205565e0c2fc83c7918d0ce6d8a9fc52a4c
Matt Johnson <majikjohnson@hotmail.com>
2019-10-24 22:54:28 +0100

    added deployment/production config

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ const app = express();
 // -- can read JSON from res.body
 app.use(express.json({extended: false}))
 
-app.get('/', (req, res) => res.json({msg: "Welcome to Contact Keeper API"}));
-
 // Define Routes
 app.use('/api/users/', require('./routes/users'));
 app.use('/api/auth/', require('./routes/auth'));
 app.use('/api/contacts/', require('./routes/contacts'));
+
+// Load static content
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 module.exports = app;
