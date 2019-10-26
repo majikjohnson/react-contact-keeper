f50854a032264798254dcf2f6dced23392674f99
Matt Johnson <majikjohnson@hotmail.com>
2019-10-26 23:46:48 +0100

    Tidied up contacts API tests

--- test/fixtures/contacts.data.js
+++ test/fixtures/contacts.data.js
@@ -13,5 +13,11 @@ module.exports = {
         name: 'Candy Cat',
 		email: 'ccat1@nascentpixels.io',
 		phone: '01234567891',
-    }
+	},
+	validContact2: {
+		name: 'Pedro Pony',
+		email: 'ppony@nascentpixels.io',
+		phone: '09876543210',
+		type: 'personal',
+	}
 };
--- test/integration/routes/contacts.test.js
+++ test/integration/routes/contacts.test.js
@@ -23,11 +23,11 @@ describe('Contacts routes', () => {
 
 	describe('POST /api/contacts', () => {
 		it('should allow a new contact to be created for given user', async () => {
+			const { name, email, phone, type } = contactsTestData.validContact;
 			const userDbJson = await mongoMemSvrHelper.insertUser(
 				userTestData.validUser
 			);
 			const token = await mongoMemSvrHelper.logInUser(userDbJson);
-			const { name, email, phone, type } = contactsTestData.validContact;
 
 			const response = await request(app)
 				.post('/api/contacts/')
@@ -41,21 +41,19 @@ describe('Contacts routes', () => {
 			response.body.should.have.property('name').and.equal(name);
 			response.body.should.have.property('email').and.equal(email);
 			response.body.should.have.property('phone').and.equal(phone);
-			response.body.should.have
-				.property('user')
-				.and.equal(userDbJson._id.toString());
-			response.body.should.have
-				.property('date')
+			response.body.should.have.property('user').and.equal(userDbJson._id.toString());
+			response.body.should.have.property('date')
 				.and.match(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/);
 		});
 
 		it('should return an error if "name" is missing', async () => {
 			const errorMessage = 'Name cannot be empty';
+
+			const { email, phone, type } = contactsTestData.validContact;
 			const userDbJson = await mongoMemSvrHelper.insertUser(
 				userTestData.validUser
 			);
 			const token = await mongoMemSvrHelper.logInUser(userDbJson);
-			const { email, phone, type } = contactsTestData.validContact;
 
 			const response = await request(app)
 				.post('/api/contacts/')
@@ -64,13 +62,12 @@ describe('Contacts routes', () => {
 				.send({ email, phone, type });
 
 			response.status.should.equal(400);
-			response.body.error[0].should.have
-				.property('msg')
-				.and.equal(errorMessage);
+			response.body.error[0].should.have.property('msg').and.equal(errorMessage);
 		});
 
 		it('should return an error if no parameters are provided', async () => {
 			const errorMessage = 'Name cannot be empty';
+
 			const userDbJson = await mongoMemSvrHelper.insertUser(
 				userTestData.validUser
 			);
@@ -82,17 +79,16 @@ describe('Contacts routes', () => {
 				.set('x-auth-token', token);
 
 			response.status.should.equal(400);
-			response.body.error[0].should.have
-				.property('msg')
-				.and.equal(errorMessage);
+			response.body.error[0].should.have.property('msg').and.equal(errorMessage);
 		});
 
 		it('should allow a new contact to be created when "name" is the only parameter provided', async () => {
+			const { name } = contactsTestData.validContact;
+
 			const userDbJson = await mongoMemSvrHelper.insertUser(
 				userTestData.validUser
 			);
 			const token = await mongoMemSvrHelper.logInUser(userDbJson);
-			const { name } = contactsTestData.validContact;
 
 			const response = await request(app)
 				.post('/api/contacts/')
@@ -103,21 +99,17 @@ describe('Contacts routes', () => {
 			response.status.should.equal(200);
 			response.body.should.have.property('_id').and.match(/^\w{24}$/);
 			response.body.should.have.property('name').and.equal(name);
-			response.body.should.have
-				.property('user')
-				.and.equal(userDbJson._id.toString());
-			response.body.should.have
-				.property('date')
+			response.body.should.have.property('user').and.equal(userDbJson._id.toString());
+			response.body.should.have.property('date')
 				.and.match(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/);
 		});
 
 		it('should set "type" to "personal" if no type parameter is provided', async () => {
-			const defaultType = 'personal';
+			const { name } = contactsTestData.validContact;
 			const userDbJson = await mongoMemSvrHelper.insertUser(
 				userTestData.validUser
 			);
 			const token = await mongoMemSvrHelper.logInUser(userDbJson);
-			const { name } = contactsTestData.validContact;
 
 			const response = await request(app)
 				.post('/api/contacts/')
@@ -126,25 +118,22 @@ describe('Contacts routes', () => {
 				.send({ name });
 
 			response.status.should.equal(200);
-			response.body.should.have.property('type').and.equal(defaultType);
+			response.body.should.have.property('type').and.equal('personal');
 		});
 	});
 
 	describe('GET /api/contacts', () => {
 		it('should return a single contact', async () => {
+			const { name, email, phone, type } = contactsTestData.validContact;
 			const userDbJson = await mongoMemSvrHelper.insertUser(
 				userTestData.validUser
 			);
-			userId = userDbJson._id.toString();
-
-			const { name, email, phone, type } = contactsTestData.validContact;
-
+			const token = await mongoMemSvrHelper.logInUser(userDbJson);
+			const userId = userDbJson._id.toString();
 			await mongoMemSvrHelper.insertContacts(
 				userId,
 				contactsTestData.validContact
 			);
-
-			const token = await mongoMemSvrHelper.logInUser(userDbJson);
 
 			const response = await request(app)
 				.get('/api/contacts/')
@@ -153,35 +142,31 @@ describe('Contacts routes', () => {
 
 			response.status.should.equal(200);
 			response.body.should.have.lengthOf(1);
-			response.body[0].should.have.property('_id').and.match(/^\w{24}$/);
-			response.body[0].should.have.property('type').and.equal(type);
-			response.body[0].should.have.property('name').and.equal(name);
-			response.body[0].should.have.property('email').and.equal(email);
-			response.body[0].should.have.property('phone').and.equal(phone);
-			response.body[0].should.have
-				.property('user')
-				.and.equal(userDbJson._id.toString());
-			response.body[0].should.have
-				.property('date')
+
+			const contact = response.body[0];
+			contact.should.have.property('_id').and.match(/^\w{24}$/);
+			contact.should.have.property('type').and.equal(type);
+			contact.should.have.property('name').and.equal(name);
+			contact.should.have.property('email').and.equal(email);
+			contact.should.have.property('phone').and.equal(phone);
+			contact.should.have.property('user').and.equal(userDbJson._id.toString());
+			contact.should.have.property('date')
 				.and.match(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/);
 		});
 
 		it('should return multiple contacts', async () => {
-			//Set up user in DB
 			const userDbJson = await mongoMemSvrHelper.insertUser(
 				userTestData.validUser
 			);
 			userId = userDbJson._id.toString();
 			const token = await mongoMemSvrHelper.logInUser(userDbJson);
-
-			//Set up contacts in DB
 			const contactsData = [
 				contactsTestData.validContact,
 				contactsTestData.validContactEmailOnly,
 				contactsTestData.validContactNoType,
 			];
 			await mongoMemSvrHelper.insertContacts(userId, contactsData);
-			contacts = await mongoMemSvrHelper.getAllContacts(userId);
+			const contacts = await mongoMemSvrHelper.getAllContacts(userId);
 
 			const response = await request(app)
 				.get('/api/contacts/')
@@ -190,22 +175,14 @@ describe('Contacts routes', () => {
 
 			response.status.should.equal(200);
 			response.body.should.have.lengthOf(3);
-
 			//Match some arbatory properties in the JSON array object
 			response.body[0].should.have.property('_id').and.match(/^\w{24}$/);
-			response.body[0].should.have
-				.property('type')
-				.and.equal(contacts[0].type);
-			response.body[1].should.have
-				.property('name')
-				.and.equal(contacts[1].name);
-			response.body[2].should.have
-				.property('email')
-				.and.equal(contacts[2].email);
+			response.body[0].should.have.property('type').and.equal(contacts[0].type);
+			response.body[1].should.have.property('name').and.equal(contacts[1].name);
+			response.body[2].should.have.property('email').and.equal(contacts[2].email);
 		});
 
 		it('should return an empty response if no contacts are available for the user', async () => {
-			//Set up user in DB
 			const userDbJson = await mongoMemSvrHelper.insertUser(
 				userTestData.validUser
 			);
@@ -222,11 +199,11 @@ describe('Contacts routes', () => {
 
 		it('should return an error if user is not logged in', async () => {
 			const errorMessage = 'No auth token.  Access Denied.';
+
 			const userDbJson = await mongoMemSvrHelper.insertUser(
 				userTestData.validUser
 			);
 			userId = userDbJson._id.toString();
-
 			await mongoMemSvrHelper.insertContacts(
 				userId,
 				contactsTestData.validContact
@@ -245,23 +222,16 @@ describe('Contacts routes', () => {
 		it('should update the contact when one field is specified', async () => {
 			const newEmail = 'newemail@nascentpixels.io';
 
-			//insert a user to DB
 			const userDbJson = await mongoMemSvrHelper.insertUser(
 				userTestData.validUser
 			);
 			userId = userDbJson._id.toString();
-
-			//log user in
 			const token = await mongoMemSvrHelper.logInUser(userDbJson);
-
-			//insert a contact ito DB for user
 			const { name, phone, type } = contactsTestData.validContact;
-
 			await mongoMemSvrHelper.insertContacts(
 				userId,
 				contactsTestData.validContact
 			);
-
 			const contact = await mongoMemSvrHelper.getAllContacts(userId);
 
 			//update the contact
@@ -273,49 +243,38 @@ describe('Contacts routes', () => {
 					email: newEmail,
 				});
 
-			//check that the response has a success status
 			response.status.should.equal(200);
-
-			//check that the provided field has been updated
+			//check that the email has been updated
 			response.body.should.have.property('email').and.equal(newEmail);
-
 			//check that the other fields are still the same
 			response.body.should.have.property('_id').and.match(/^\w{24}$/);
 			response.body.should.have.property('type').and.equal(type);
 			response.body.should.have.property('name').and.equal(name);
 			response.body.should.have.property('phone').and.equal(phone);
-			response.body.should.have
-				.property('user')
-				.and.equal(userDbJson._id.toString());
-			response.body.should.have
-				.property('date')
+			response.body.should.have.property('user').and.equal(userDbJson._id.toString());
+			response.body.should.have.property('date')
 				.and.match(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/);
 		});
 
 		it('should update the contact when all fields are specified', async () => {
 			//setup contact update values
-			const newName = 'Pedro Pony';
-			const newEmail = 'ppony@nascentpixels.io';
-			const newPhone = '09876543210';
-			const newType = 'personal';
+			const newName = contactsTestData.validContact2.name;
+			const newEmail = contactsTestData.validContact2.email;
+			const newPhone = contactsTestData.validContact2.phone;
+			const newType = contactsTestData.validContact2.type;
 
-			//insert a user in DB
 			const userDbJson = await mongoMemSvrHelper.insertUser(
 				userTestData.validUser
 			);
 			userId = userDbJson._id.toString();
-
-			//log user in
 			const token = await mongoMemSvrHelper.logInUser(userDbJson);
-
-			//insert a contact into DB for user
+			//create a contact for the user that we will update
 			await mongoMemSvrHelper.insertContacts(
 				userId,
 				contactsTestData.validContact
 			);
 			const contact = await mongoMemSvrHelper.getAllContacts(userId);
 
-			// Make update API call
 			const response = await request(app)
 				.put(`/api/contacts/${contact[0]._id}`)
 				.set('Content-Type', 'application/json')
@@ -327,43 +286,31 @@ describe('Contacts routes', () => {
 					type: newType,
 				});
 
-			//check that the response has a success status
 			response.status.should.equal(200);
-
-			//check that the provided fields has been updated
+			//check that the provided properties has been updated
 			response.body.should.have.property('email').and.equal(newEmail);
 			response.body.should.have.property('type').and.equal(newType);
 			response.body.should.have.property('name').and.equal(newName);
 			response.body.should.have.property('phone').and.equal(newPhone);
-
 			//check that the other fields are still correct
-			response.body.should.have
-				.property('_id')
-				.and.equal(contact[0]._id.toString());
-			response.body.should.have
-				.property('user')
-				.and.equal(contact[0].user.toString());
-			response.body.should.have
-				.property('date')
-				.and.equal(contact[0].date.toISOString());
+			response.body.should.have.property('_id').and.equal(contact[0]._id.toString());
+			response.body.should.have.property('user').and.equal(contact[0].user.toString());
+			response.body.should.have.property('date').and.equal(contact[0].date.toISOString());
 		});
 
 		it('should return a "not found" error if the given contact doesn\'t exist', async () => {
-			//setup test specific data values
-			const missingContactId = '000000000000000000000000';
-			const newEmail = 'newemail@nascentpixels.io';
 			const errorMessage = 'Contact not found';
 
-			//insert a user in DB
+			//setup data values
+			const missingContactId = '000000000000000000000000';
+			const newEmail = 'newemail@nascentpixels.io';
+
 			const userDbJson = await mongoMemSvrHelper.insertUser(
 				userTestData.validUser
 			);
 			userId = userDbJson._id.toString();
-
-			//log user in
 			const token = await mongoMemSvrHelper.logInUser(userDbJson);
 
-			//Attempt to update the contact
 			const response = await request(app)
 				.put(`/api/contacts/${missingContactId}`)
 				.set('Content-Type', 'application/json')
@@ -372,22 +319,20 @@ describe('Contacts routes', () => {
 					email: newEmail,
 				});
 
-			//check that the response has correct status and error message
 			response.status.should.equal(404);
 			response.body.should.have.property('msg').and.equal(errorMessage);
 		});
 
 		it('should return a "not authorised" error if the user is not logged in', async () => {
-			//Set up test specific data
-			const newEmail = 'ppony@nascentpixels.io';
 			const errorMessage = 'No auth token.  Access Denied.';
 
-			//insert a user in DB
+			//Set up contact data
+			const newEmail = contactsTestData.validContact2.email;
+
 			const userDbJson = await mongoMemSvrHelper.insertUser(
 				userTestData.validUser
 			);
 			userId = userDbJson._id.toString();
-
 			//insert a contact into DB for user
 			await mongoMemSvrHelper.insertContacts(
 				userId,
@@ -395,7 +340,6 @@ describe('Contacts routes', () => {
 			);
 			const contact = await mongoMemSvrHelper.getAllContacts(userId);
 
-			//Attempt to update the contact
 			const response = await request(app)
 				.put(`/api/contacts/${contact[0]._id.toString()}`)
 				.set('Content-Type', 'application/json')
@@ -403,7 +347,6 @@ describe('Contacts routes', () => {
 					email: newEmail,
 				});
 
-			//check that the response has correct status and error message
 			response.status.should.equal(401);
 			response.body.should.have.property('msg').and.equal(errorMessage);
 		});
@@ -411,16 +354,11 @@ describe('Contacts routes', () => {
 
 	describe('DELETE /api/contacts/:id', () => {
 		it('should only delete the given contact', async () => {
-			//insert a user to DB
 			const userDbJson = await mongoMemSvrHelper.insertUser(
 				userTestData.validUser
 			);
 			userId = userDbJson._id.toString();
-
-			//log user in
 			const token = await mongoMemSvrHelper.logInUser(userDbJson);
-
-			//insert a contact into DB for user
 			contactsData = [
 				contactsTestData.validContact,
 				contactsTestData.validContactEmailOnly,
@@ -431,115 +369,86 @@ describe('Contacts routes', () => {
 				userId
 			);
 
-			//Delete the contact
 			const response = await request(app)
 				.delete(`/api/contacts/${originalContacts[0]._id}`)
 				.set('Content-Type', 'application/json')
 				.set('x-auth-token', token);
 
-			//check that the response has a success status
 			response.status.should.equal(200);
-			response.body.should.have
-				.property('msg')
-				.and.equal('Contact Deleted');
-
+			response.body.should.have.property('msg').and.equal('Contact Deleted');
 			//Check that the correct contact has been deleted from the database
-			const updatedContacts = await mongoMemSvrHelper.getAllContacts(
-				userId
-			);
+			const updatedContacts = await mongoMemSvrHelper.getAllContacts(userId);
 			updatedContacts.should.have.lengthOf(2);
 			updatedContacts[0].should.be.eql(originalContacts[1]);
 			updatedContacts[1].should.be.eql(originalContacts[2]);
 		});
 
 		it('should allow the only contact to be deleted', async () => {
-			//insert a user to DB
 			const userDbJson = await mongoMemSvrHelper.insertUser(
 				userTestData.validUser
 			);
-			userId = userDbJson._id.toString();
-
-			//log user in
 			const token = await mongoMemSvrHelper.logInUser(userDbJson);
-
-			//insert a contact into DB for user
+			userId = userDbJson._id.toString();
 			await mongoMemSvrHelper.insertContacts(
 				userId,
 				contactsTestData.validContact
 			);
 			let contact = await mongoMemSvrHelper.getAllContacts(userId);
 
-			//Delete the contact
 			const response = await request(app)
 				.delete(`/api/contacts/${contact[0]._id}`)
 				.set('Content-Type', 'application/json')
 				.set('x-auth-token', token);
 
-			//check that the response has a success status
 			response.status.should.equal(200);
-			response.body.should.have
-				.property('msg')
-				.and.equal('Contact Deleted');
-
+			response.body.should.have.property('msg').and.equal('Contact Deleted');
 			//Check that the contact has been deleted from the database
 			contact = await mongoMemSvrHelper.getAllContacts(userId);
 			contact.should.be.empty;
 		});
 
 		it('should return a "not found" error if the given contact doesn\'t exist', async () => {
-			//setup test specific data values
-			const missingContactId = '000000000000000000000000';
 			const errorMessage = 'Contact not found';
 
-			//insert a user to DB
+			const missingContactId = '000000000000000000000000';
+
 			const userDbJson = await mongoMemSvrHelper.insertUser(
 				userTestData.validUser
 			);
 			userId = userDbJson._id.toString();
-
-			//log user in
 			const token = await mongoMemSvrHelper.logInUser(userDbJson);
-
 			//insert a contact into DB for user
 			await mongoMemSvrHelper.insertContacts(
 				userId,
 				contactsTestData.validContact
 			);
 
-			//delete the contact
 			const response = await request(app)
 				.delete(`/api/contacts/${missingContactId}`)
 				.set('Content-Type', 'application/json')
 				.set('x-auth-token', token);
 
-			//check that the response has a success status
 			response.status.should.equal(404);
 			response.body.should.have.property('msg').and.equal(errorMessage);
 		});
 
 		it('should return a "not authorised" error if the user is not logged in', async () => {
-			//setup test specific data values
 			const errorMessage = 'No auth token.  Access Denied.';
 
-			//insert a user to DB
 			const userDbJson = await mongoMemSvrHelper.insertUser(
 				userTestData.validUser
 			);
 			userId = userDbJson._id.toString();
-
-			//insert a contact into DB for user
 			await mongoMemSvrHelper.insertContacts(
 				userId,
 				contactsTestData.validContact
 			);
-			let contact = await mongoMemSvrHelper.getAllContacts(userId);
+			const contact = await mongoMemSvrHelper.getAllContacts(userId);
 
-			//attempt to delete the contact
 			const response = await request(app)
 				.delete(`/api/contacts/${contact[0]._id}`)
 				.set('Content-Type', 'application/json');
 
-			//check that the response has correct status and error message
 			response.status.should.equal(401);
 			response.body.should.have.property('msg').and.equal(errorMessage);
 		});
